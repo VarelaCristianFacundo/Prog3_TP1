c5db1bee4dc6a84f4553c2ca0318cae327228d66
VarelaCristianFacundo <cfacundovarela@yahoo.com>
2015-09-09 17:55:49 -0300

    se crea el archivo mostrar, donde se utiliza el explode

---
+++ Mostrar.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once ("Persona.php");
+require_once ("Empleado.php");
+require_once ("Fabrica.php");
+
+	$ar = fopen("Empleados.txt", "r");
+	while (!feof($ar)) 
+	{
+	//	echo fgets($ar), "<br/>";
+		$prueba = fgets($ar);
+		$pruebaDos = explode("-", $prueba);
+		if (count($pruebaDos) >1)
+		{
+
+			$miEmpleadoPrueba = new Empleado ($pruebaDos[0], $pruebaDos[1], $pruebaDos[2], $pruebaDos[3], $pruebaDos[4], $pruebaDos[5]);
+			echo $miEmpleadoPrueba->Tostring()."<br>";
+		}
+		echo "<br>".count($pruebaDos);
+
+	}
+
+	fclose($ar);
+
+?>
